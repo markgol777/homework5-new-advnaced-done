478041d12762e37ebe904c7ede5ce152fa32fde8
markgol777 <markgol777@gmail.com>
2022-01-01 15:42:23 +0200

    added all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { FirstHomeworkComponent } from './work/first-homework/first-homework.component';
+import { SecondHomeworkComponent } from './work/second-homework/second-homework.component';
+import { ThirdHomeworkComponent } from './work/third-homework/third-homework.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkComponent,
+    FirstHomeworkComponent,
+    SecondHomeworkComponent,
+    ThirdHomeworkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/work/third-homework/third-homework.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ThirdHomeworkComponent } from './third-homework.component';
+
+describe('ThirdHomeworkComponent', () => {
+  let component: ThirdHomeworkComponent;
+  let fixture: ComponentFixture<ThirdHomeworkComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ThirdHomeworkComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ThirdHomeworkComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
